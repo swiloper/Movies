52877469975b606d6513cf76e38bfcdb867d7d59
swiloper <myronishyn.ihor@icloud.com>
2023-09-04 17:40:11 +0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Ihor Myronishyn on 04.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
